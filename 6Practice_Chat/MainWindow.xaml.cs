911a7420fa837092ad0feee70ee923071a30aefd
SergeyPoltev <isip.s.d.poltev@mpt.ru>
2023-05-06 23:57:31 +0300

    проект

---
+++ 6Practice_Chat/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics.Eventing.Reader;
+using System.Timers;
+using MaterialDesignThemes.Wpf;
+
+namespace _6Practice_Chat
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Timer timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (InputName.Text != "")
+                {
+                    TextFieldAssist.SetUnderlineBrush(InputName, Brushes.GreenYellow);
+                }
+                else
+                {
+                    TextFieldAssist.SetUnderlineBrush(InputName, Brushes.DarkRed);
+                }
+            });
+        }
+    }
+}
